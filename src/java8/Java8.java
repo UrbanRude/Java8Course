e140f77bfed5004b826d03d7975c42a823920e3d
JAVA4 <urban.rude@gmail.com>
2018-07-03 16:05:28 -0500

    Method References

--- src/java8/Java8.java
+++ src/java8/Java8.java
@@ -5,53 +5,26 @@
  */
 package java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Java8 {
 
     final static String saludation = "Hello! ";
     
     public static void main(String[] args) {
+        List namesList = new ArrayList();
+        
+        namesList.add("Enero");
+        namesList.add("Febrero");
+        namesList.add("Marzo");
+        namesList.add("Abril");
+        namesList.add("Mayo");
+        namesList.add("Junio");
+        
+        namesList.forEach(System.out::println);
         
         
-        Java8 java = new Java8();
-        
-        MathOperation addition = ( int a, int b ) -> a + b;
-        
-        MathOperation subtraction = ( a, b ) -> a - b;
-        
-        MathOperation multiplication = ( int a, int b ) -> { return a*b; };
-        
-        MathOperation division = ( int a, int b ) -> a/b ;
-        
-        System.out.println("10 + 5 = " + java.operate( 5, 10, addition ));
-        System.out.println("10 - 5 = " + java.operate( 10, 5, subtraction ));
-        System.out.println("10 * 5 = " + java.operate( 5, 10, multiplication ));
-        System.out.println("10 / 5 = " + java.operate( 10, 5, division ));
-        
-        GreetingService greetingService = message -> System.out.println("Hello" + message);
-        
-        greetingService.sayMessage("Urbano");
-        greetingService.sayMessage("Ceron");
-        
-        
-        GreetingService greetingService1 = message -> { 
-            System.out.println("Mensaje pasado >> "+message);
-            System.out.println( saludation + message);
-        };
-        
-        greetingService1.sayMessage("Urban");
-         
     }
-    
-    interface MathOperation{
-        int operation( int a, int b );
-    }
-    
-    interface GreetingService{
-        void sayMessage(String message);
-    }
-    
-    private int operate( int a, int b, MathOperation mathOperation){
-        return mathOperation.operation(a, b);
-    }
-    
+       
 }
